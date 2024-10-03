81b109b04a37d1e23c53f747f5217e2c5d3a9339
ej-shafran <ejshafran@hilma.tech>
2024-10-04 00:44:49 +0300

    refactor: remove parsing/hashing/stringifying logic for now

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,1 @@
-import * as game from "./game";
-
-const pre = document.createElement("pre");
-pre.textContent = JSON.stringify(game.parse("01111"));
-document.querySelector("#app")?.appendChild(pre);
+console.log("Hello, world");
