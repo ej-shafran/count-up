c7898ecc9372e1636893473a3ff7b6a98ebf7e32
ej-shafran <ejshafran@hilma.tech>
2024-10-03 23:11:15 +0300

    refactor: rename result import and use specific types

--- src/game.ts
+++ src/game.ts
@@ -1,31 +1,62 @@
-import * as Result from "./result";
+import * as result from "./result";
 
 export const HAND_AMOUNT = 2;
 export const PLAYER_AMOUNT = 2;
-export const MAX_COUNT = 5;
+export const MAX_COUNT = 4;
 
 type SizedArray<TItem, TLength extends number> = readonly TItem[] & {
   readonly length: TLength;
 };
 
-type RangeImpl<
+type RangeInclusiveImpl<
+  TMax extends number,
+  TAcc extends 1[] = [],
+> = TAcc["length"] extends TMax
+  ? TMax
+  : TAcc["length"] | RangeInclusiveImpl<TMax, [...TAcc, 1]>;
+
+type RangeExclusiveImpl<
   TMax extends number,
   TAcc extends 1[] = [],
 > = TAcc["length"] extends TMax
   ? never
-  : TAcc["length"] | RangeImpl<TMax, [...TAcc, 1]>;
+  : TAcc["length"] | RangeExclusiveImpl<TMax, [...TAcc, 1]>;
 
-type Range<TMax extends number> = RangeImpl<TMax>;
+type RangeImpl<
+  TMax extends number,
+  TExclusive extends boolean | undefined,
+> = number extends TMax
+  ? number
+  : TExclusive extends true
+    ? RangeExclusiveImpl<TMax>
+    : RangeInclusiveImpl<TMax>;
 
-type Hand = Range<typeof MAX_COUNT>;
+type Range<
+  TOptions extends {
+    max: number;
+    isExclusive?: boolean;
+  },
+> = RangeImpl<TOptions["max"], TOptions["isExclusive"]>;
+
+export type HandValue = Range<{
+  max: typeof MAX_COUNT;
+}>;
+export type HandIndex = Range<{
+  max: typeof HAND_AMOUNT;
+  isExclusive: true;
+}>;
+export type PlayerIndex = Range<{
+  max: typeof PLAYER_AMOUNT;
+  isExclusive: true;
+}>;
 
 export interface Player {
-  readonly hands: SizedArray<Hand, typeof HAND_AMOUNT>;
+  readonly hands: SizedArray<HandValue, typeof HAND_AMOUNT>;
 }
 
 export interface Game {
   readonly players: SizedArray<Player, typeof PLAYER_AMOUNT>;
-  readonly current: number;
+  readonly current: PlayerIndex;
 }
 
 export const initial: Game = {
@@ -35,7 +66,7 @@ export const initial: Game = {
 
 export function hash(game: Game): number {
   return [
-    game.current,
+    game.current as number,
     ...game.players.flatMap((player) =>
       [...player.hands].sort((a, b) => a - b),
     ),
@@ -69,18 +100,18 @@ export class ParseError {
   }
 }
 
-export function parse(raw: string): Result.Result<Game, ParseError> {
+export function parse(raw: string): result.Result<Game, ParseError> {
   const currentRaw = raw[0];
   if (!currentRaw)
-    return Result.fail(new ParseError(ParseErrorReason.EMPTY_STRING));
+    return result.fail(new ParseError(ParseErrorReason.EMPTY_STRING));
 
   const current = parseInt(currentRaw);
   if (isNaN(current) || current < 0 || current >= PLAYER_AMOUNT)
-    return Result.fail(new ParseError(ParseErrorReason.INVALID_CURRENT_PLAYER));
+    return result.fail(new ParseError(ParseErrorReason.INVALID_CURRENT_PLAYER));
 
   const playerAmount = (raw.length - 1) / HAND_AMOUNT;
   if (playerAmount !== PLAYER_AMOUNT)
-    return Result.fail(new ParseError(ParseErrorReason.INVALID_PLAYER_AMOUNT));
+    return result.fail(new ParseError(ParseErrorReason.INVALID_PLAYER_AMOUNT));
 
   const players = Array.from({ length: PLAYER_AMOUNT }, (_, i) => ({
     hands: [
@@ -94,19 +125,16 @@ export function parse(raw: string): Result.Result<Game, ParseError> {
         player.hands.length !== HAND_AMOUNT || player.hands.some(isNaN),
     )
   ) {
-    return Result.fail(new ParseError(ParseErrorReason.INVALID_HAND_AMOUNT));
+    return result.fail(new ParseError(ParseErrorReason.INVALID_HAND_AMOUNT));
   }
 
   if (
     players.some((player) =>
-      player.hands.some((hand) => hand < 0 || hand >= MAX_COUNT),
+      player.hands.some((hand) => hand < 0 || hand > MAX_COUNT),
     )
   ) {
-    return Result.fail(new ParseError(ParseErrorReason.INVALID_HAND_VALUE));
+    return result.fail(new ParseError(ParseErrorReason.INVALID_HAND_VALUE));
   }
 
-  return Result.succeed({
-    current,
-    players: players as Game["players"],
-  });
+  return result.succeed({ current, players } as Game);
 }
