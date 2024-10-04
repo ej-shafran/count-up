11076eab9ed533cfb64793bfc31145bceffa5e7a
ej-shafran <ejshafran@hilma.tech>
2024-10-04 05:40:30 +0300

    feat: add helpful texts

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,24 @@
 import clsx from "clsx";
 import * as game from "../game";
 import { Hand } from "./Hand";
+import {
+  useCanSplit,
+  useCurrentPlayer,
+  useIsClickable,
+  useLoser,
+} from "../game/store";
 
 export interface PlayerProps {
   playerIndex: game.PlayerIndex;
 }
 
 export function Player({ playerIndex }: PlayerProps) {
+  const currentPlayer = useCurrentPlayer();
+  const canSplit = useCanSplit(playerIndex);
+  const isClickable = useIsClickable(playerIndex);
+  const otherIsClickable = useIsClickable(playerIndex === 0 ? 1 : 0);
+  const loser = useLoser();
+
   return (
     <div
       className={clsx(
@@ -15,7 +27,21 @@ export function Player({ playerIndex }: PlayerProps) {
       )}
     >
       <h2 className="arvo-regular text-xl">
-        Player {playerIndex === 1 ? "Two" : "One"}
+        {(loser !== null || currentPlayer !== playerIndex) && (
+          <>Player {playerIndex === 1 ? "Two" : "One"}</>
+        )}
+
+        {loser === null && currentPlayer === playerIndex && (
+          <>
+            {isClickable && (
+              <>Select a hand to use{canSplit && " (or SPLIT)"}</>
+            )}
+
+            {otherIsClickable && (
+              <>Select a hand to target{canSplit && " (or SPLIT)"}</>
+            )}
+          </>
+        )}
       </h2>
 
       <div className="flex grow gap-3">
