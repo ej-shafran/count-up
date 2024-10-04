79e872f1b79d9fa06b5847d4ba7cceaf7b7eb68e
ej-shafran <ejshafran@hilma.tech>
2024-10-04 17:24:40 +0300

    fix: add responsiveness

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,7 +3,7 @@ import { GameOverPopup } from "./GameOverPopup";
 
 export function Game() {
   return (
-    <div className="m-auto flex h-full w-1/2 flex-col py-3 text-center">
+    <div className="m-auto flex size-full flex-col p-3 text-center lg:w-1/2 lg:px-0">
       <h1 className="arvo-bold text-4xl">Counting Game</h1>
       <div className="flex grow flex-col gap-3">
         <Player player={1} />
