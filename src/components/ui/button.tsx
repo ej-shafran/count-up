9a08899384ff0ac09e48a85215ce2f732f30b0b1
ej-shafran <ejshafran@hilma.tech>
2024-10-13 10:34:07 +0300

    chore: fix linting issues

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "size-10",
       },
     },
     defaultVariants: {
@@ -55,4 +55,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
+// eslint-disable-next-line react-refresh/only-export-components -- shadcn file; does not need fast refresh
 export { Button, buttonVariants };
